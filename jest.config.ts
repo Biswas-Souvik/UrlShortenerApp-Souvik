d3c88c36761740a62cd5d68b0b194b4b4bcd066d
Souvik Biswas <biswassouvik284@gmail.com>
2025-12-30 19:08:22 +0530

    chore: add husky pre-commit hooks to check test coverage above 50

--- jest.config.ts
+++ jest.config.ts
@@ -2,4 +2,13 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   setupFilesAfterEnv: ['./tests/jest.setup.ts'],
+  collectCoverageFrom: ['src/**/*.ts', '!src/**/*.d.ts'],
+  coverageThreshold: {
+    global: {
+      branches: 50,
+      functions: 50,
+      lines: 50,
+      statements: 50,
+    },
+  },
 };
